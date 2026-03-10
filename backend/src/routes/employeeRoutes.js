413fb36e16d6778d0483838733a36d37d106d2bf
quangduy164 <duyhx164@gmail.com>
2026-03-10 17:06:27 +0700

    update middleware

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const employeeController = require("../controllers/employeeController");
+const { verifyToken } = require("../middleware/authMiddleware");
 
-router.post("/employees", employeeController.createEmployee);
-router.get("/employees", employeeController.getEmployees);
-router.get("/employees/:id", employeeController.getEmployeeById);
-router.put("/employees/:id", employeeController.updateEmployee);
-router.delete("/employees/:id", employeeController.deleteEmployee);
+router.post("/employees", verifyToken, employeeController.createEmployee);
+router.get("/employees", verifyToken, employeeController.getEmployees);
+router.get("/employees/:id", verifyToken, employeeController.getEmployeeById);
+router.put("/employees/:id", verifyToken, employeeController.updateEmployee);
+router.delete("/employees/:id", verifyToken, employeeController.deleteEmployee);
 
 module.exports = router;
