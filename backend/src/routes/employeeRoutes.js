064329fcf93c3b38ff33e7da2f1883f2a8083a36
quangduy164 <duyhx164@gmail.com>
2026-03-12 10:21:25 +0700

    update unit test coverage

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const employeeController = require("../controllers/employeeController");
 const { verifyToken } = require("../middleware/authMiddleware");
 
-router.post("/employees", verifyToken, employeeController.createEmployee);
+router.post("/employees", employeeController.createEmployee);
 router.get("/employees", verifyToken, employeeController.getEmployees);
 router.get("/employees/:id", verifyToken, employeeController.getEmployeeById);
 router.put("/employees/:id", verifyToken, employeeController.updateEmployee);
