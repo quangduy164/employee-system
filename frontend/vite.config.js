c3b4ed61aba767fc0ec7768b0854fdb25bce2723
quangduy164 <duyhx164@gmail.com>
2026-03-13 13:47:04 +0700

    fix eslint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -19,4 +19,4 @@ export default defineConfig({
     setupFiles: "./src/tests/setup.js",
     globals: true
   }
-})
+});
