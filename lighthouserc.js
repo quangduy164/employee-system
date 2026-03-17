a0a83586a0794ee266012762c1e8a16a9292d0e7
quangduy164 <duyhx164@gmail.com>
2026-03-17 14:56:30 +0700

    update

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,15 +1,18 @@
 module.exports = {
   ci: {
     collect: {
-      chromePath: "/usr/bin/chromium",
       url: [
         "https://tqa-test.deha.vn",
         "https://tqa-test.deha.vn/home"
       ],
       numberOfRuns: 1,
+
       puppeteerScript: "lighthouse-login.js",
-      settings: {
-        chromeFlags: "--headless --no-sandbox --disable-gpu"
+
+      chromePath: "/usr/bin/chromium-browser",
+
+      puppeteerLaunchOptions: {
+        args: ["--no-sandbox", "--disable-gpu"]
       }
     }
   }
